3642407c06523776bbd2ae3d4a3cefd5cfc6f9e3
MohammedAlHasa1 <>
2021-07-15 11:34:48 +0000

    Add css code

---
+++ app.js
@@ -0,0 +1,26 @@
+var phoneName = prompt("what is your fav iphone ?")
+
+alert("Nice choice " + phoneName )
+
+var search = prompt("What's the phone that you want search about it ?")
+
+if(search == "iphone 5"){
+document.write(
+  "<div>" + "<h3>"+"Phone you search about :"  + search+"</h3>")
+
+}
+
+else if (search =="iphone 7"){
+document.write(
+  "<div>" + "<h3>"+"Phone you search about :"  + search+"</h3>")
+
+}
+else if(search =="iphone 8"){
+document.write(
+  "<div>" + "<h3>"+"Phone you search about :" + search+"</h3>")
+
+}
+
+else{
+  alert('This phone is not in the website')
+}
